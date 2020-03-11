eb173cd40df163d8c73d9a32efb3384aaef8b3e7
maguowei <imaguowei@gmail.com>
2020-03-11 15:24:50 +0800

    fix app crash

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import frida
 from flask import Flask, jsonify, request
 from hook import start_hook
 
@@ -8,9 +9,15 @@ api = start_hook()
 
 @app.route('/sign')
 def sign():
+    global api
     url = request.args.get('url', '')
     headers = dict(request.headers)
-    data = api.exports.sign(url, headers)
+    try:
+        data = api.exports.sign(url, headers)
+    except frida.InvalidOperationError as e:
+        print(e)
+        api = start_hook()
+        data = api.exports.sign(url, headers)
     return jsonify({
         'url': url,
         'headers': headers,
